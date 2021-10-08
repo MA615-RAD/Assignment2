96da7526c8d8e1f4a7ce36921e49d1c7ab5c0ead
radonahue <radona@bu.edu>
2021-10-08 09:39:46 -0400

    Hopefully should be the final commit for this assignment

--- visualization_function.R
+++ visualization_function.R
@@ -1,27 +1,36 @@
-library(tidyverse)
+#The plotting function below takes in a year and a tibble as arguments and makes a scatter plot of Employment Percentages and the Number of Children per women. It labels the scatter dots with the Country name that contain either a maximum or minimum for those variables (so the country that has the most or least number of children per woman, the country that has the most or least percentage of young women employed). 
 
-
-#Function takes in a year and a tibble as arguments, to make a scatter plot of employment percentage for women aged 15-24 versus fertility rates for a particular year.
 
 plot <- function(a, b) {
   {
     year<-a
     data2<-filter(b, year==a)
+    #The first thing it does is filters the dataset for the year that is taken in by the function and it uses the filter function from the dplyr package
     t<-max(data2$children, na.rm=T)
-    data2$test<-ifelse(data2$children==t, data2$country, "")
-    c<-ggplot(data=data2, aes(x=employpercent, y=children))+geom_point(color="#F3A8EF")+ ylim(0,9) + geom_text(aes(label=test),hjust=0, vjust=0) + ggtitle("Fertility Versus Employment Percentage for", year) + labs(x="Employment Percentage of Women Ages 15-24", y="Number of Children")+
+    z<-max(data2$employpercent, na.rm=T)
+    x<-min(data2$children, na.rm=T)
+    y<-min(data2$employpercent, na.rm=T)
+    #it then calculates the mean and the minimum of each of the variables, ignores blanks in the data
+    data2$test<-ifelse(data2$children==t|data2$employpercent==z|data2$children==x|data2$employpercent==y, data2$country, "")
+    #creates a column in the filtered dataset that grabs the country name if a row contained the max or min value of the statistic and leaves the row blank if it doesn't. This is for the conditional labeling in the next section of the countries
+    c<-ggplot(data=data2, aes(x=employpercent, y=children))+geom_point(color="#F3A8EF")+ ylim(0,9) + geom_text(aes(label=test),hjust=0.5, vjust=0) + ggtitle("Fertility Versus Employment Percentage for", year) + labs(x="Employment Percentage of Women Ages 15-24", y="Number of Children")+
 theme_minimal()
+    #Creates a scatter plot, gives it an html color scheme, sets the y axis to be fixed so it's easier to compare trends year over year, labels only the four countries that take on the max and min values from the conditional column, gives it a title that tells you what year you're looking at, and labels the x and y axis, and gives it a theme
     print(c)
   }
 }
 
+#For reproducibility purposes, an opportunity to improve this code would be to avoid hard coding in x and y variables, that way generic datasets could be fed into the function, however the titles and labels would have to be modified since that is specific to whichever dataset is being used
+
+#For fun I just wanted to make a plot for each year for the last 30 years to watch how the scatterplot eventually becomes more concentrated towards the lower end of each axis, I set the for loop to run backwards so the first plot that comes up is 1990 and you have to tab backwards to see the next year (kind of like a flip book). Only the years 1990 and 2020 will be included in the next step to talk about comparisons between two subsets of data.
+
+years<-2020:1990
+
+for (year in years) {
+  plot(year, data)
+}
 
 
-plot(1990, data)
-
-View(data2)
-
-plot(2020, data)
 
 
 
--- wrangling_code.R
+++ wrangling_code.R
@@ -1,23 +1,35 @@
 #Load tidyverse and excel packages
+
 library(tidyverse)
 library(openxlsx)
-#Reading in the 2 datasets, saved in the project file
 
+#Reading in the 2 datasets that were downloaded from (https://www.gapminder.org/data/, saved in the R project file to be read in directly
+
+Children1<-read.xlsx("children_per_woman_total_fertility.xlsx")
+View(Children1)
+#I can see that this dataset has a lot of years, some that take place in the future. I have decided to omit those for my analysis since I'm only interested in summarizing what has happened. The column names and the datatypes seem to be suffiencient and don't require any extra cleanup
 
 Children<-read.xlsx("children_per_woman_total_fertility.xlsx", cols=1:223)
-#putting contraints on which columns to read since I'm not interested in future projections, this data set contains years that haven't happened yet
-Employment<-read.xlsx("females_aged_15_24_employment_rate_percent.xlsx")
+#Reading in the dataset again and omitting the future years
 
+Employment<-read.xlsx("females_aged_15_24_employment_rate_percent.xlsx")
+View(Employment)
+
+#Reading in this data as is, I'm content with the data and it doesn't seem to require any additional trimming or cleaning
 
 child<-Children %>% pivot_longer(!country, names_to = "year", values_to = "children")
 
+#Using the tidyr package, the pivot longer function takes in the first data set and turns each of the columns that contain a year into individual rows (a combination of year and country name make a unique identifier for the row) data set now has 3 columns of country, year, and average number of children 
 
 
 employ<-Employment %>% pivot_longer(!country, names_to = "year", values_to = "employpercent")
 
+#same process as above, column names are year, country, and percentage of women aged 15-24 employed
+
+
 
 data<-left_join(child, employ, by=c("year", "country"))
-#joining the two data sets together, expects blank values in the employpercent column since there are several years where there is no employment data for 
+#To combine the two datasets together, I decided to use a left join function from the dplyr package since Children has many more countries and years available than Employment does, so this will bring in all the rows from Children and Employment and leave blanks in the cells for the Employment stats where data is missing. I will set calculations that I do in the next section to ignore the blanks in the relevant columns. I joined on year and country since that is what creates a unique identifier per row for this data. 
 
 is_tibble(data)
-
+#Verifying that dataset is a tibble for the next section, this returns True
